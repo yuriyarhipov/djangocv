c643dbbcde9ebe90ce0cf9f6ab6ca26f0b2334ef
arhipov <arhipov@localhost.localdomain>
2020-03-02 17:34:19 +0300

    add information field

--- cv/models.py
+++ cv/models.py
@@ -5,3 +5,7 @@ class Cv(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
+    information = models.TextField(default='')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
