0c3c5a24e0bd052e9345336e00407d52ed7f05f2
arhipov <arhipov@localhost.localdomain>
2020-03-05 17:05:52 +0300

    add several inforamtion fields

--- cv/admin.py
+++ cv/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from cv.models import Cv
+from cv.models import Cv, Technologies
 
 admin.site.register(Cv)
+admin.site.register(Technologies)
--- cv/models.py
+++ cv/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
 
+class Technologies(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
 class Cv(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
     information = models.TextField(default='')
+    email = models.EmailField(default='')
     phone = models.CharField(max_length=50, default='')
     github = models.URLField(default='')
     linkedin = models.URLField(default='')
+    technologies = models.ManyToManyField(Technologies)
 
 
     def __str__(self):
