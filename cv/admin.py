c2967584a2616b200c4e991979f185ca121fae0f
arhipov <arhipov@localhost.localdomain>
2020-03-11 01:24:45 +0300

    add filter to admin for points

--- cv/admin.py
+++ cv/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from cv.models import Cv, Technologies, Education, Experience, Points
 
+
+class PointsAdmin(admin.ModelAdmin):
+    list_filter = ('experience__title', )
+    list_display = ('title', 'experience')
+
+
 admin.site.register(Cv)
 admin.site.register(Technologies)
 admin.site.register(Education)
 admin.site.register(Experience)
-admin.site.register(Points)
+admin.site.register(Points, PointsAdmin)
