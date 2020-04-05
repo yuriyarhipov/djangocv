61ae81e1c48ab338c8ebf69bd0d05ada96a2faed
Yuriy Arhipov <yuriyarhipov@gmail.com>
2020-04-05 17:16:20 +0300

    move to pg

--- djangocv/settings.py
+++ djangocv/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'djangocv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mypass',
+        'HOST': 'db',
+        'PORT': 5432,
+        'DATABASE': 'db',
     }
 }
 
