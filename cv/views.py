dad798a7e366c00277fe68363e7ce63f6223e0f8
Yuriy Arhipov <yuriyarhipov@gmail.com>
2020-04-22 16:43:00 +0300

    fix year

--- cv/views.py
+++ cv/views.py
@@ -1,7 +1,11 @@
+import datetime 
+
 from django.shortcuts import render
 from cv.models import Cv
 
 
 def main(request):
     data = Cv.objects.filter().first()
-    return render(request, 'main.html', {'data': data})
+    result = {'data': data}
+    result['year'] = datetime.datetime.now().year    
+    return render(request, 'main.html', result)
